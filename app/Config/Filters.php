50f921893746bccf1bd69230ba43a8d59250ac4d
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-09-08 09:53:34 -0300

    cors

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -55,6 +55,7 @@ class Filters extends BaseFilters
         'before' => [
             'forcehttps', // Force Global Secure Requests
             'pagecache',  // Web Page Caching
+            'cors'
         ],
         'after' => [
             'pagecache',   // Web Page Caching
